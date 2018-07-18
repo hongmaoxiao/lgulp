7be3386185a83c89feb63da98c980ef1b102f83c
fengxiaomao <fxm@autoforce.net>
2018-07-18 16:45:35 +0800

    gulp.src with 'since' option

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify');
 
 gulp.task('default', defaultTask);
 
 function defaultTask(done) {
+  var now = new Date();
+  var since = new Date(+now - 10 * 60 * 1000);
+
+  gulp.src('source/*.js', { since: since })
+    .pipe(minify())
+    .pipe(gulp.dest('build'));
+
   done();
 }
