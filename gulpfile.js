3588464add8a6cf42d96f87757a1450f128c3d5c
fengxiaomao <fxm@autoforce.net>
2018-07-19 11:55:32 +0800

    return a stream and watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
 
-gulp.task('default', defaultTask);
-
-function defaultTask(done) {
-  // var now = new Date();
-  // var since = new Date(+now - 10 * 60 * 1000);
-
-  gulp.src('src/**/*.js', { cwd: '../otherbase' })
-    .pipe(minify())
-    .pipe(gulp.dest('build'));
-
-  done();
+var paths = {
+  scripts: {
+    src: 'source/js/ee/**/*.js',
+    dest: 'dist/js'
+  }
 }
 
-defaultTask.description = 'It is a default function';
+// gulp.task('default', defaultTask);
+// gulp.task('default', res_stream);
+
+// function defaultTask(done) {
+//   // var now = new Date();
+//   // var since = new Date(+now - 10 * 60 * 1000);
+
+//   gulp.src('src/**/*.js', { cwd: '../otherbase' })
+//     .pipe(minify())
+//     .pipe(gulp.dest('build'));
+
+//   done();
+// }
+
+// defaultTask.description = 'It is a default function';
+
+function watch() {
+  gulp.watch(paths.scripts.src, res_stream);
+}
+
+function res_stream() {
+  return gulp.src(paths.scripts.src)
+  .pipe(minify())
+  .pipe(gulp.dest(paths.scripts.dest));
+}
+
+gulp.task('default', watch);
